54d649ae9f823ed0fc53b5db404aeeb9c8aef72c
Andres Felipe Campuzano Cervantes <andrescampu1@gmail.com>
2022-04-28 22:17:46 -0500

    Corrección del modal de eliminar

--- src/components/ModalEliminar.jsx
+++ src/components/ModalEliminar.jsx
@@ -14,14 +14,14 @@ const ModalEliminar = ({ showModal, handleModal, Imagen, creados, setCreados }) 
         <>
             <Modal show={showModal} onHide={handleModal}>
                 <Modal.Header closeButton>
-                    <h5>Comprar Imagen</h5>
+                    <h5>Elimnar Imagen</h5>
                 </Modal.Header>
                 <Modal.Body>
                     <label htmlFor="imagen" className='col-12 fw-bold my-2'>Seguro que quiere Eliminar la siguiente imagen?</label>
                     <img id="imagen" src={Imagen.imagen} alt="Imagen" className="mx-auto d-block" />
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button onClick={EliminarFoto} className="btn-danger btn-sm mx-2  fw-bold">Confirmar</Button>
+                    <Button onClick={EliminarFoto} className="btn-danger btn-sm mx-2  fw-bold">Eliminar</Button>
                     <Button onClick={handleModal} className="btn-dark btn-sm mx-2  fw-bold">Cerrar</Button>
                 </Modal.Footer>
             </Modal>
